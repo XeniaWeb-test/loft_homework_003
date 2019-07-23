42c2b1ee87d6fcba09caa7d58c735dfe10845ada
XeniaTkacheva <xenia1898@gmail.com>
2019-07-23 17:33:00 +0700

    Добавляет задание #1

--- src/functions.php
+++ src/functions.php
@@ -2,4 +2,41 @@
 /**
  * Задание #1
  *
+ * @param string $fileName
  */
+
+function task1($fileName)
+{
+    ob_start();
+    $data = simplexml_load_file($fileName);
+
+    foreach ($data->attributes() as $attr) {
+        echo $attr->getName() . ': ' . $attr . PHP_EOL . PHP_EOL;
+    }
+
+    foreach ($data->Address as $key => $val) {
+        echo $val->getName() . PHP_EOL;
+        echo $val->attributes()->getName() .': ' . $val->attributes()->Type  . PHP_EOL;
+        echo  $val->Name->getName() . ': ' . $val->Name->__toString() . PHP_EOL;
+        $addr = [];
+        $addr[] = $val->Street->__toString();
+        $addr[] = $val->City->__toString();
+        $addr[] = $val->State->__toString();
+        $addr[] = $val->Zip->__toString();
+        $addr[] = $val->Country->__toString();
+        echo implode(', ', $addr) . PHP_EOL . PHP_EOL;
+    }
+
+    echo 'DeliveryNotes: ' . $data->DeliveryNotes->__toString() . PHP_EOL . PHP_EOL;
+
+    foreach ($data->Items->Item as $item => $product) {
+        echo $product->attributes()->getName() . ': ' . $product->attributes() . PHP_EOL;
+        echo $product->ProductName->__toString() . ' ' . $product->Quantity->__toString() . ' pcs' . PHP_EOL;
+        echo 'Price ' . $product->USPrice->__toString() . ' US$' . PHP_EOL;
+        echo 'Comments: ' . $product->Comment->__toString() . PHP_EOL;
+        echo 'Shipping Date: ' . $product->ShipDate->__toString() . PHP_EOL . PHP_EOL;
+    }
+    $content = ob_get_contents();
+    ob_end_clean();
+    echo nl2br($content);
+}
